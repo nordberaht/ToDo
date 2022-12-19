3d657d9e1a57822640e3d1fb4b04c95dac2f8e04
nordberaht <northberaht@gmail.com>
2022-12-19 07:45:11 +0100

    Task view align text fix

--- src/components/List/taskView.js
+++ src/components/List/taskView.js
@@ -7,7 +7,7 @@ const TaskView = (props) => {
       <div className={styles["backdrop"]} onClick={props.closeTask}></div>
       <div className={styles["outter"]}>
         <div className={styles["inner"]}>
-          <p>{props.task.dateValue}</p>
+          <p className={styles.date}>{props.task.dateValue}</p>
           <h3>{props.task.titleValue}</h3>
           <p className={styles.description}>{props.task.descriptionValue}</p>
         </div>
